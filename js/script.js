d3ace5c5a675e6c2920e758ce3169c7dbcfca12e
unknown <mayanktomar1607.gmail.com>
2019-12-23 17:24:39 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function likec(){
+  var ele=document.getElementById('liketext');
+  ele.style.color="white";
+  var ele1=document.getElementById('likebtn');
+  ele1.style.backgroundColor="blue";
+
+}
